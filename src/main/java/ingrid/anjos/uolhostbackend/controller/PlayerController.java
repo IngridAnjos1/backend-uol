df703f36a8c081ea1d249c3c149ceac009f19d26
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-03-14 15:54:05 -0300

    feat: inclusao do metodo createPlayer no controller

--- src/main/java/ingrid/anjos/uolhostbackend/controller/PlayerController.java
+++ src/main/java/ingrid/anjos/uolhostbackend/controller/PlayerController.java
@@ -1,4 +1,27 @@
 package ingrid.anjos.uolhostbackend.controller;
 
+
+import ingrid.anjos.uolhostbackend.model.Player;
+import ingrid.anjos.uolhostbackend.model.dtos.PlayerDto;
+import ingrid.anjos.uolhostbackend.service.PlayerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/players")
 public class PlayerController {
+
+  @Autowired
+  private PlayerService service;
+
+  public ResponseEntity<Player> createPlayer(@RequestBody @Valid PlayerDto dto) {
+    Player newPlayer = service.createPlayer(dto);
+    return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
+  }
 }
+
