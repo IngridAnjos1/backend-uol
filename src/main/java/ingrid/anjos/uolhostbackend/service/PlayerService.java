bb10b2cc58a7038536f67cc69cdf05096feaa01d
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-03-19 18:42:33 -0300

    feat: inclusao de anotação

--- src/main/java/ingrid/anjos/uolhostbackend/service/PlayerService.java
+++ src/main/java/ingrid/anjos/uolhostbackend/service/PlayerService.java
@@ -1,18 +1,38 @@
 package ingrid.anjos.uolhostbackend.service;
 
+import ingrid.anjos.uolhostbackend.infra.CodinameHandler;
+import ingrid.anjos.uolhostbackend.model.GroupType;
 import ingrid.anjos.uolhostbackend.model.Player;
 import ingrid.anjos.uolhostbackend.model.dtos.PlayerDto;
 import ingrid.anjos.uolhostbackend.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
 @Service
 public class PlayerService {
   @Autowired
   private PlayerRepository repository;
 
-  public Player createPlayer(PlayerDto dto){
+  @Autowired
+  private CodinameHandler handler;
+
+  public Player createPlayer(PlayerDto dto) {
     Player newPlayer = new Player(dto);
+    String codiname = getCodiname(dto.groupType());
+    newPlayer.setCodiname(codiname);
     return repository.save(newPlayer);
   }
+
+  private String getCodiname(GroupType groupType){
+    return handler.findCodiname(groupType);
+  }
+
+  public List<Player> getAllPlayers() {
+    return repository.findAll();
+  }
 }
+
+
